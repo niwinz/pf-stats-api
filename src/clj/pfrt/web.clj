1ff228ec8acb1312acfc55f943a7e72a6da10750
Andrey Antukh <niwi@niwi.be>
2013-12-24 19:06:01 +0100

    Add port configuration to config.edn file.

--- src/clj/pfrt/web.clj
+++ src/clj/pfrt/web.clj
@@ -1,12 +1,15 @@
 (ns pfrt.web
   (:require [compojure.handler :as handler]
             [org.httpkit.server :as hkit]
-            [pfrt.web.routes :refer [app]]))
+            [pfrt.web.routes :refer [app]]
+            [pfrt.service :as service]))
 
 (defn s-web
   "Main runnable entry for start a standalone
   web server."
   []
-  (let [ctrl (handler/api app)]
-    (println "Listening: http://localhost:9090/")
-    (hkit/run-server ctrl {:port 9090})))
+  (let [ctrl (handler/api app)
+        cfg  (service/cfg)
+        port (get cfg :port 9090)]
+    (println (format "Listening: http://localhost:%s/" port))
+    (hkit/run-server ctrl {:port port})))
